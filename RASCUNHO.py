7cafba393b1d4a8a7b8daeb7dc75115aa8981cef
maprevidelli <marco.previdelli@gmail.com>
2025-04-30 19:25:57 -0300

    atuliza aula 76

--- RASCUNHO.py
+++ RASCUNHO.py
@@ -1,11 +1,8 @@
-numeros = [1, 2, 3, 4, 5]
+# Criar dicionário com quadrados dos números
+quadrados = {x: x**2 for x in range(1, 6)}
+print(quadrados)  # {1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
 
-def quadrado(x):
-    return x ** 2
-
-# Aplicando map
-quadrados = list(map(quadrado, numeros))
-print(quadrados)  # Saída: [1, 4, 9, 16, 25]
-
-# Com lambda (função anônima)
-quadrados = list(map(lambda x: x**2, numeros))
+# Filtrar itens
+pessoa = {"nome": "Ana", "idade": 25, "cidade": "Rio"}
+adulto = {chave: valor for chave, valor in pessoa.items() if chave != "cidade"}
+print(adulto)  # {'nome': 'Ana', 'idade': 25}
